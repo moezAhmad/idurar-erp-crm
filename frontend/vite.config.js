7e354d76052bee353aeeb8c4b83401b1368b1d6c
moezAhmad <moez.ahmad.dev@gmail.com>
2024-11-06 14:31:42 +0500

    Updated prod urls

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://localhost:8888/';
+      : process.env.VITE_BACKEND_SERVER;
 
   const config = {
     plugins: [react()],
