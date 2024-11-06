740532f955cee88de4fa3008e98a56b362aba591
moezAhmad <moez.ahmad.dev@gmail.com>
2024-11-06 14:36:53 +0500

    Updated frontend

--- frontend/src/forms/LoginForm.jsx
+++ frontend/src/forms/LoginForm.jsx
@@ -51,6 +51,15 @@ export default function LoginForm() {
           {translate('Forgot password')}
         </a>
       </Form.Item>
+
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>{translate('Remember me')}</Checkbox>
+        </Form.Item>
+        <a className="login-form-forgot" href="/forgetpassword" style={{ marginLeft: '0px' }}>
+          {translate('Forgot password')}
+        </a>
+      </Form.Item>
     </div>
   );
 }
